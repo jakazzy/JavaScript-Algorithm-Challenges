d9bc9ddc5b58c8210283908b4661a67dc5a56837
jida <jakazzy@gmail.com>
2019-05-06 09:01:22 +0000

    add caesar's cipher

--- index.js
+++ index.js
@@ -45,3 +45,45 @@ var removeDuplicates = function(nums) {
     }
     return ++i;
 };
+
+//Caesar's cipher - Freecode camp
+function rot13(str) { // LBH QVQ VG!
+    convertToRot13 = {
+        "A": "N",
+        "B": "O",
+        "C": "P",
+        "D": "Q",
+        "E": "R",
+        "F": "S",
+        "G": "T",
+        "H": "U",
+        "I": "V",
+        "J": "W",
+        "K": "X",
+        "L": "Y",
+        "M": "Z",
+        "N": "A",
+        "O": "B",
+        "P": "C",
+        "Q": "D",
+        "R": "E",
+        "S": "F",
+        "T": "G",
+        "U": "H",
+        "V": "I",
+        "W": "J",
+        "X": "K",
+        "Y": "L",
+        "Z": "M"
+    };
+    let letters = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] in convertToRot13) {
+            letters = letters + convertToRot13[str[i]];
+        } else letters = letters + str[i];
+    }
+    return letters;
+}
+
+// Change the inputs below to test
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.");
